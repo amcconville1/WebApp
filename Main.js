c290dc6f296c7c5d857e3626415dae8f91aa01f7
Alexander McConville <alexander.mcconville@pubsubsandbox.onmicrosoft.com>
2021-07-02 19:03:15 -0700

    Updated packages to include wiki api, fixed query url.

--- Main.js
+++ Main.js
@@ -1,8 +1,7 @@
 // Kaden's Comment
-// Please see http://localhost:3000 when running.
 var request = require('request');
-var query = 'Node.js';
-var url = `https://en.wikipedia.org/w/api.php?action=opensearch&search="+ ${query} +”&format=json`;
+var query = 'English';
+var url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json`;
 request(url, function(err, response, body) {
     if (err) {
         var error = "cannot connect to server";
